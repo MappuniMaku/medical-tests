5595acf23dadb4a34d0a187ce91f2d0d97a66086
Grigory Telenok <telenok.grigory@gmail.com>
2026-05-10 00:30:30 +1000

    Add biology 3 topic

--- src/data/biology/index.ts
+++ src/data/biology/index.ts
@@ -1,5 +1,6 @@
 import { ITopic } from '../../types';
 import { biology1 } from './1';
 import { biology2 } from './2';
+import { biology3 } from './3';
 
-export const biology: ITopic[] = [biology1, biology2];
+export const biology: ITopic[] = [biology1, biology2, biology3];
